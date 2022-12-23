1d15e17edb736a5faa488e79b7519ed8cbd616a4
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2022-12-23 23:51:22 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,23 @@
+import PageHOC from './PageHOC' ; 
+import CustomInput from './CustomInput';
+import CustomButton from './CustomButton';
+import Alert from './Alert' ; 
+import GameLoad from './GameLoad';
+import GameInfo from './GameInfo';
+import Card from './Card';
+import ActionButton from './ActionButton';
+import PlayerInfo from './PlayerInfo';
+import OnboardModel from './OnboardModal'; 
+
+export {
+    PageHOC,
+    CustomInput,
+    CustomButton,
+    Alert,
+    GameLoad,
+    GameInfo,
+    ActionButton,
+    PlayerInfo,
+    Card,
+    OnboardModel
+}
---
+++ client/src/contract/index.js
@@ -0,0 +1,5 @@
+import contract from './AVAXGods.json' ; 
+
+export const ADDRESS="0xEeBBb2B73D346bD45a777D9A95F2Eda956003E89";
+
+export const {abi: ABI} = contract ; 
